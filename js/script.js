defe6eb4504b62a1f904f40dfdcd17e0a0250401
Yuri Gianlorenzi <yurigian@gmail.com>
2021-01-29 00:26:27 +0100

    add move to list from trash (recycle)

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ var app = new Vue ({
     deleteTodo (todoIndex) {
       //qui usiamo come argomento della funzione l'index dell'elemento, questo verrà passato al metodo splice, che elimina esattamente l'elemento con quell'index
       alert('Il messaggio verrà spostato nel cestino');
+      //ATTENZIONE ALL'ORDINE: PRIMA SI PUSHA E POI SI ELIMINA/SPOSTA
       this.deletedList.push(this.todoList[todoIndex]);
       //nelle parentesi, prima specifico l'indice dell'elemento su cui lavorare, poi specifico QUANTI ELEMENTI devo eliminare oltre a quello. (se metto 2 elimina quello dell'index e il successivo, se metto 3 elimina i 2 successivi....SE METTO 0, LO AGGIUNGE)
       this.todoList.splice(todoIndex, 1);
@@ -26,8 +27,13 @@ var app = new Vue ({
     deleteForver (deletedIndex) {
       alert('Il messaggio verrà definitivamente eliminato');
       this.deletedList.splice(deletedIndex, 1);
-    }//fine deletedForever
+    },//fine deletedForever
 
+    moveToList (deletedIndex) {
+      //qui viene riciclato un elemento todo, viene pushato nella todolist l'elemento selezionato, dopo essere stato pushato
+      this.todoList.push(this.deletedList[deletedIndex]);
+      this.deletedList.splice(deletedIndex, 1);
+    }
 
   }//fine methods
 });

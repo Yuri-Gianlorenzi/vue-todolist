cf50092e9286541612561964376984e1f7686ec5
Yuri Gianlorenzi <yurigian@gmail.com>
2021-01-28 17:16:21 +0100

    add v-for in html to create the list from the two array in data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 var app = new Vue ({
   el : '#app',
   data : {
-
+    todoList : [
+      'creare il logo',
+      'creare la lista todo',
+      'creare la lista deleted',
+      'implementare tulle le funzioni possibili e immaginabili'
+    ],
+    deletedList : [
+      'creare il logo',
+      'creare la lista todo',
+      'creare la lista deleted',
+      'implementare tulle le funzioni possibili e immaginabili'
+    ]
 
   },//fine data
 

4f871188ac4031bd365250a8132cb596708f8d6f
Yuri Gianlorenzi <yurigian@gmail.com>
2021-01-28 18:12:51 +0100

    add v-if/v-else for both empty arrays

--- js/script.js
+++ js/script.js
@@ -19,10 +19,13 @@ var app = new Vue ({
   methods : {
     deleteTodo (todoIndex) {
       //qui usiamo come argomento della funzione l'index dell'elemento, questo verrà passato al metodo splice, che elimina esattamente l'elemento con quell'index
+      alert('Il messaggio verrà spostato nel cestino');
+      //nelle parentesi, prima specifico l'indice dell'elemento su cui lavorare, poi specifico quanti elementi devo eliminare oltre a quello. (se metto 2 elimina quello dell'index e il successivo, se metto 3 elimina i 2 successivi....)
       this.todoList.splice(todoIndex, 1);
     },//fine delete todo
 
     deleteForver (deletedIndex) {
+      alert('Il messaggio verrà definitivamente eliminato');
       this.deletedList.splice(deletedIndex, 1);
     }//fine deletedForever
   }//fine methods

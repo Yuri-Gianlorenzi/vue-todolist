db5338ec7f969e6080555230a653bb7bb87c6d50
Yuri Gianlorenzi <yurigian@gmail.com>
2021-01-29 00:15:08 +0100

    add move to trash function

--- js/script.js
+++ js/script.js
@@ -8,10 +8,7 @@ var app = new Vue ({
       'implementare tulle le funzioni possibili e immaginabili'
     ],
     deletedList : [
-      'creare il logo',
-      'creare la lista todo',
-      'creare la lista deleted',
-      'implementare tulle le funzioni possibili e immaginabili'
+
     ]
 
   },//fine data
@@ -20,13 +17,17 @@ var app = new Vue ({
     deleteTodo (todoIndex) {
       //qui usiamo come argomento della funzione l'index dell'elemento, questo verrà passato al metodo splice, che elimina esattamente l'elemento con quell'index
       alert('Il messaggio verrà spostato nel cestino');
-      //nelle parentesi, prima specifico l'indice dell'elemento su cui lavorare, poi specifico quanti elementi devo eliminare oltre a quello. (se metto 2 elimina quello dell'index e il successivo, se metto 3 elimina i 2 successivi....)
+      this.deletedList.push(this.todoList[todoIndex]);
+      //nelle parentesi, prima specifico l'indice dell'elemento su cui lavorare, poi specifico QUANTI ELEMENTI devo eliminare oltre a quello. (se metto 2 elimina quello dell'index e il successivo, se metto 3 elimina i 2 successivi....SE METTO 0, LO AGGIUNGE)
       this.todoList.splice(todoIndex, 1);
+      console.log(todoIndex);
     },//fine delete todo
 
     deleteForver (deletedIndex) {
       alert('Il messaggio verrà definitivamente eliminato');
       this.deletedList.splice(deletedIndex, 1);
     }//fine deletedForever
+
+
   }//fine methods
 });

b55437d2d978225db7c88aff205331eaade0bbd6
Yuri Gianlorenzi <yurigian@gmail.com>
2021-01-29 01:30:50 +0100

    add move all todolist to trash function

--- js/script.js
+++ js/script.js
@@ -5,6 +5,14 @@ var app = new Vue ({
       'creare il logo',
       'creare la lista todo',
       'creare la lista deleted',
+      'implementare tulle le funzioni possibili e immaginabili',
+      'creare il logo',
+      'creare la lista todo',
+      'creare la lista deleted',
+      'implementare tulle le funzioni possibili e immaginabili',
+      'creare il logo',
+      'creare la lista todo',
+      'creare la lista deleted',
       'implementare tulle le funzioni possibili e immaginabili'
     ],
     deletedList : [
@@ -17,7 +25,7 @@ var app = new Vue ({
     deleteTodo (todoIndex) {
       //qui usiamo come argomento della funzione l'index dell'elemento, questo verrà passato al metodo splice, che elimina esattamente l'elemento con quell'index
       alert('Il messaggio verrà spostato nel cestino');
-      //ATTENZIONE ALL'ORDINE: PRIMA SI PUSHA E POI SI ELIMINA/SPOSTA
+      //ATTENZIONE ALL'ORDINE: PRIMA SI PUSHA E POI SI ELIMINA/SPOSTA, si potrebbe utilizzare una variabile
       this.deletedList.push(this.todoList[todoIndex]);
       //nelle parentesi, prima specifico l'indice dell'elemento su cui lavorare, poi specifico QUANTI ELEMENTI devo eliminare oltre a quello. (se metto 2 elimina quello dell'index e il successivo, se metto 3 elimina i 2 successivi....SE METTO 0, LO AGGIUNGE)
       this.todoList.splice(todoIndex, 1);
@@ -33,7 +41,18 @@ var app = new Vue ({
       //qui viene riciclato un elemento todo, viene pushato nella todolist l'elemento selezionato, dopo essere stato pushato
       this.todoList.push(this.deletedList[deletedIndex]);
       this.deletedList.splice(deletedIndex, 1);
-    }
+    },//fine moveToList
+
+    moveToTrash () {
+      //qui spostiamo tutti gli elementi della todoList nella deletedList, usiamo ciclo for della lunghezza dell'array todoList, pushando uno a uno gli elementi nell'array deletedList
+      for (let i = 0; i < this.todoList.length; i++) {
+        this.deletedList.push(this.todoList[i]);
+      }
+
+      // qui rimuoviamo gli elementi dall'array todoList usando splice. SI PUÒ SVUOTARE UN ARRAY  dandogli posizione zero, e un numero di elementi da eliminare pari alla lunghezza dell'array. partirà ad eliminare dal primo elemento (indice 0)
+      this.todoList.splice(0, this.todoList.length);
+
+    }//fine moveToTrash
 
   }//fine methods
 });
